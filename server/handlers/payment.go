cfb7d975a661a320b3cbcdb5abd886f2872a92ae
Abush Natanem <abushnatanem@gmail.com>
2025-08-13 12:17:25 +0300

    logic for the handling payment checkout

---
+++ server/handlers/payment.go
@@ -0,0 +1,81 @@
+package handlers
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "checkout_sdk/crypto"
+    "checkout_sdk/models"
+)
+
+var (
+    merchantID    = os.Getenv("MERCHANT_ID")
+    encryptionKey = os.Getenv("ENCRYPTION_KEY")
+    postURL       = os.Getenv("POST_URL")
+)
+
+func PreparePayment(w http.ResponseWriter, r *http.Request) {
+    var req models.PaymentRequest
+    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+        http.Error(w, "Invalid request", http.StatusBadRequest)
+        return
+    }
+
+    // VARIABLES WE NEED FOR THE TXN DETAILS
+    agID := "yagout"
+    country := "ETH"
+    currency := "ETB"
+    txnType := "SALE"
+    channel := "WEB"
+    isLoggedIn := "Y"
+
+
+    // VARIABLES WE NEED
+    pgDetails := "|||"
+    cardDetails := "||||"
+    upiDetails := ""
+    otherDetails := "||||"
+    billDetails := "||||"
+    shipDetails := "||||||"
+    itemDetails := "||"
+
+    custDetails := fmt.Sprintf("%s|%s|%s||%s", req.CustName, req.Email, req.Mobile, isLoggedIn)
+
+    txnDetails := fmt.Sprintf("%s|%s|%s|%s|%s|%s|%s|%s|%s|%s",
+        agID, merchantID, req.OrderNo, req.Amount, country, currency, txnType, req.SuccessURL, req.FailureURL, channel)
+
+    allValues := txnDetails + "~" + pgDetails + "~" + cardDetails + "~" + custDetails + "~" + billDetails + "~" + shipDetails + "~" + itemDetails + "~" + upiDetails + "~" + otherDetails
+
+    merchantRequest, err := crypto.Encrypt(allValues, encryptionKey)
+    if err != nil {
+        http.Error(w, "Encryption error", http.StatusInternalServerError)
+        return
+    }
+
+    hash := crypto.GenerateHash(merchantID, merchantRequest)
+
+    response := map[string]string{
+        "me_id":            merchantID,
+        "merchant_request": merchantRequest,
+        "hash":             hash,
+        "post_url":         postURL,
+    }
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+
+func HandleSuccess(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    merchantResponse := r.FormValue("merchant_response")
+    decrypted, err := crypto.Decrypt(merchantResponse, encryptionKey)
+    if err != nil {
+        http.Error(w, "Decryption error", http.StatusInternalServerError)
+        return
+    }
+    fmt.Fprintf(w, "<h1>Payment Successful</h1><p>Details: %s</p>", decrypted)
+}
+
+func HandleFailure(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "<h1>Payment Failed</h1>")
+}
