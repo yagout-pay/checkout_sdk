c0326ca314113121eaf8d88f6ebd85111c314d4e
Abush Natanem <abushnatanem@gmail.com>
2025-08-13 13:09:19 +0300

    update the main file

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,4 +1,31 @@
 package main
-func main () {
 
+import (
+	"checkout_sdk/handlers"
+	"fmt"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+    if err := godotenv.Load(); err != nil {
+        fmt.Println("No .env file found")
+    }
+
+    http.HandleFunc("/", helloWorld)
+
+    http.HandleFunc("/api/prepare-payment", handlers.PreparePayment)
+    http.HandleFunc("/success", handlers.HandleSuccess)
+    http.HandleFunc("/failure", handlers.HandleFailure)
+    fmt.Println("Backend running on :8080")
+    http.ListenAndServe(":8080", nil)
 }
