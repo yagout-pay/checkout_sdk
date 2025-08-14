0d7c6e086514b5da4ae0cdf9dec2cd18e3cb9954
Abush Natanem <abushnatanem@gmail.com>
2025-08-15 00:27:26 +0300

    changes

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -16,16 +16,33 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "https://d22e169bb385.ngrok-free.app")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-    if err := godotenv.Load(); err != nil {
-        fmt.Println("No .env file found")
-    }
 
-    http.HandleFunc("/", helloWorld)
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found")
+	}
+	mux := http.NewServeMux()
 
-    http.HandleFunc("/api/prepare-payment", handlers.PreparePayment)
-    http.HandleFunc("/success", handlers.HandleSuccess)
-    http.HandleFunc("/failure", handlers.HandleFailure)
-    fmt.Println("Backend running on :8080")
-    http.ListenAndServe(":8080", nil)
+	mux.HandleFunc("/", helloWorld)
+	mux.HandleFunc("/api/prepare-payment", handlers.PreparePayment)
+	// mux.HandleFunc("/success", handlers.HandleSuccess)
+	// mux.HandleFunc("/failure", handlers.HandleFailure)
+
+    fmt.Println("Backend running on http://localhost:3000")
+	http.ListenAndServe(":3000", corsMiddleware(mux))
 }
